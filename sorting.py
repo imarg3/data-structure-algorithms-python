e403b2db849c9679737ee737e3775db1f5875b0a
Arpit Gupta <imarg_3@zoho.com>
2020-09-14 23:59:29 +0530

    Add insert sort and improve bubble sort function

--- sorting.py
+++ sorting.py
@@ -23,11 +23,66 @@ def bubble_sort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
 
+def bubble_sort_improved(arr):
+    # run n-1 passes for bubble sort
+    for i in range(len(arr) - 1):
+        # compare all consecutive elements of the array
+        for j in range(len(arr) - 1 - i):
+            # if j th element > j+1 th element, swap them
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+
+def bubble_sort_improved_further(arr):
+    # run n-1 passes for bubble sort
+    for i in range(len(arr) - 1):
+        flag = True
+        # compare all consecutive elements of the array
+        for j in range(len(arr) - 1 - i):
+            # if j th element > j+1 th element, swap them
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+        if not flag:
+            break
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        temp = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > temp:
+            arr[j + 1] = arr[j]
+            j = j - 1
+        arr[j + 1] = temp
+
+
 def main():
     arr = [25, 14, 22, 43, 68, 14]
-    # selection_sort(arr)
+    selection_sort(arr)
+    print("Selection Sort")
+    print(arr)
+    print()
+
+    print("Bubble Sort")
     bubble_sort(arr)
     print(arr)
+    print()
+
+    print("Bubble Sort Improved")
+    bubble_sort_improved(arr)
+    print(arr)
+    print()
+
+    print("Bubble Sort Improved Further")
+    bubble_sort_improved_further(arr)
+    print(arr)
+    print()
+
+    print("Insertion Sort")
+    insertion_sort(arr)
+    print(arr)
+    print()
 
 
 if __name__ == "__main__":
