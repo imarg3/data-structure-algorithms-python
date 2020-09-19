82485b8cd145be5a1f96b12c1ee88a9746da22db
Arpit Gupta <imarg_3@zoho.com>
2020-09-20 01:12:01 +0530

    Singly List ADT implementation

--- singlylist.py
+++ singlylist.py
@@ -38,6 +38,74 @@ class SinglyList:
             traverse = traverse.next
         print()
 
+    # time complexity - O(n)
+    def add_at_pos(self, val, pos):
+        # spl 4. invalid pos( < 1)
+        if pos < 1:
+            raise Exception("Invalid position " + str(pos))
+        # spl 1. if list is empty OR spl 2. add at pos = 1
+        if self.head is None or pos == 1:
+            self.add_first(val)
+        else:
+            # create a new node
+            new_node = Node(val)
+            # traverse till pos - 1
+            traverse = self.head
+            for i in range(1, pos - 1):
+                # spl 3. if pos to add is beyond length of list, throw error
+                traverse = traverse.next
+                if traverse is None:
+                    raise Exception("Invalid position " + str(pos))
+            # add node at that pos
+            new_node.next = traverse.next
+            traverse.next = new_node
+
+    # time complexity - O(1)
+    def del_first(self):
+        if self.head is None:
+            raise Exception("List is empty.")
+        self.head = self.head.next
+
+    # time complexity - O(n) or O(pos)
+    def del_at_pos(self, pos):
+        # spl 3: if pos < 1, throw exception
+        if pos < 1:
+            raise Exception("Invalid position " + str(pos))
+        # spl 1: list is empty & 2: pos == 1
+        if self.head is None or pos == 1:
+            self.del_first()
+        else:
+            traverse = self.head
+            for i in range(1, pos-1):
+                traverse = traverse.next
+                # spl 4: if pos > length, throw exception
+                if traverse is None:
+                    raise Exception("Invalid position " + str(pos))
+            temp = traverse.next
+            # spl 5: pos == length, if temp is null, throw exception
+            if temp is None:
+                raise Exception("Invalid position " + str(pos))
+            traverse.next = temp.next
+
+    # time complexity - O(n)
+    def del_all(self):
+        while self.head is not None:
+            self.del_first()
+        # head = null;
+
+    # time complexity - O(n)
+    def del_last(self):
+        # spl1: list is empty, spl2: list have only one node.
+        if self.head is None or self.head.next is None:
+            self.del_first()
+        else:
+            temp = None
+            traverse = self.head
+            while traverse.next is not None:
+                temp = traverse
+                traverse = traverse.next
+            temp.next = None
+
 
 def main():
     list1 = SinglyList()
@@ -45,6 +113,14 @@ def main():
     list1.add_last(20)
     list1.add_last(30)
     list1.add_first(40)
+    list1.display()
+    list1.add_at_pos(50, 2)
+    list1.display()
+    list1.del_first()
+    list1.display()
+    list1.del_last()
+    list1.display()
+    list1.del_at_pos(3)
     list1.display()
 
 
